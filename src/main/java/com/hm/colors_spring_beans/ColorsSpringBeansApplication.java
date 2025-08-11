d937a475031e3b45c87e640cf67ce59c703e8008
HazemMasarani <hazemmasarani99@gmail.com>
2025-08-10 20:52:51 -0400

    Colors Project

---
+++ src/main/java/com/hm/colors_spring_beans/ColorsSpringBeansApplication.java
@@ -0,0 +1,13 @@
+package com.hm.colors_spring_beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorsSpringBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorsSpringBeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hm/colors_spring_beans/ColorsSpringBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.colors_spring_beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorsSpringBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
